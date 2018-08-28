b5f2aa8407785c487062d9c12e3fefb89a38ba53
Joao S O Bueno <gwidion@gmail.com>
2018-08-27 22:58:48 -0300

    Minimal usable normalizer

--- data_plot.py
+++ data_plot.py
@@ -1,14 +1,24 @@
 
-def normalizer(data):
-    min_data = min(data)
-    amplitude = max(data) - min_data
 
-    offset = min_data if min_data >= 0 else -min_data
+class NormalIter:
+    def __init__(self, data, scale=1):
+        self.data = data
+        self.min_data = min(data)
+        self.max_data = max(data)
 
-    return offset, scale_factor
+        self.amplitude = self.max_data - self.min_data
+        self.normalize_factor = 1 / self.amplitude
 
+        self.offset = self.min_data if self.min_data >= 0 else -self.min_data
+        self.scale = scale
+
+    def __iter__(self):
+        for item in self.data:
+            yield (item - self.offset) * self.normalize_factor * self.scale
 
 
 
 def scatter(plot_func, size, data1, data2):
     width, height = size
+    for x, y in zip(NormalIter(data1, width), NormalIter(data2, height)):
+        plot_func(x, y)
